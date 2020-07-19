f128844745cd3f014a10326df55a4c052519ea43
James <jamesoghlanian@gmail.com>
2020-07-19 00:47:15 -0400

    updated about

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => (
     <div id="about-text">
         <h4>How it works?</h4>
-        <div>This Recipe bot uses Google's Natural Language Processing platform, Dialogflow, and picks up on key phrases in the conversation to suggest tasty (in our opinion!) recipes. It uses Dialogflow intents and events to simulate a natural conversation with the user.</div>
+        <div>This Recipe bot uses Google's Natural Language Processing platform, Dialogflow, and picks up on key phrases in the conversation to suggest tasty (in our opinion!) recipes. It uses Dialogflow intents and events to simulate a natural conversation with the user. The Recipe chatbot is a work in progress and will continue to be updated. Your feedback is appreciated and you can reach me <a href="mailto:jamesoghlanian@gmail.com">here</a>.</div>
         <img id="about-img" src="https://images.unsplash.com/photo-1585109649139-366815a0d713?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
     </div>    
 )
